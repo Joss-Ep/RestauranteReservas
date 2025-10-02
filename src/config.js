f5b2c5bfc9710ea9d794390c0c6ab0100dfa0824
JOSSSEPH YAURI <87624141+Joss-Ep@users.noreply.github.com>
2025-10-02 01:17:13 -0500

    Update config.js

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 const config = {
   port: process.env.PORT || 3000,
-  databaseUrl: process.env.DATABASE_URL,
+  databaseUrl: process.env.DATABASE_URL='postgresql://neondb_owner:npg_6moBkiQsACY4@ep-noisy-paper-adtn3ltq-pooler.c-2.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require',
   pgssl: (process.env.PGSSL || 'true').toLowerCase() === 'true',
   jwtSecret: process.env.JWT_SECRET || 'change_me',
   jwtExpiresIn: process.env.JWT_EXPIRES_IN || '7d',
